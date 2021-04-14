374a16d7c6464324a3e91bda3fd6fc088e073a92
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-04-14 15:29:07 -0300

    ORM mapping, database seeding

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,14 @@
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Tiago', 'Brown', 'tiago@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Sandra', 'Green', 'sandra@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+
 INSERT INTO tb_client(name, cpf, income,birth_Date,children) VALUES ('Maria Silva','12345678901',6500.0, TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z',2);
 INSERT INTO tb_client(name, cpf, income,birth_Date,children) VALUES ('Antonio Souza','81316298051',3500.0, TIMESTAMP WITH TIME ZONE '1980-07-20T10:30:00Z',2);
 INSERT INTO tb_client(name, cpf, income,birth_Date,children) VALUES ('Tiago Baratieri','87353264098',2500.0, TIMESTAMP WITH TIME ZONE '1950-07-20T10:30:00Z',2);
